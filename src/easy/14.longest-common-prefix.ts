3c5e5f08b56d72da177c474da2200e6cd21e44bc
hjb89 <hjb89@126.com>
2019-04-19 10:20:56 +0800

    初步通过版
    Your runtime beats 77.74 % of javascript submissions
      √ Your memory usage beats 22.65 % of javascript submissions (35.4 MB)

---
+++ src/easy/14.longest-common-prefix.ts
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=javascript
+ *
+ * [14] 最长公共前缀
+ *
+ * https://leetcode-cn.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (32.81%)
+ * Total Accepted:    71.2K
+ * Total Submissions: 217K
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ *
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1:
+ *
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ *
+ *
+ * 说明:
+ *
+ * 所有输入只包含小写字母 a-z 。
+ *
+ */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs: string[]) {
+  if (strs.length === 0) {
+    return ""
+  } else {
+    const findMinStr = (strs: string[]): string => {
+      let minStr = strs[0]
+      let minLength = strs[0].length
+      for (let str of strs) {
+        if (str.length < minLength) {
+          minStr = str
+          minLength = str.length
+        }
+      }
+      return minStr
+    }
+    const minLengthStr = findMinStr(strs)
+    let prefix = ""
+    for (let index = 0; index < minLengthStr.length; index++) {
+      let isEqual = true
+      const temporaryPrefix = minLengthStr.slice(0, index + 1)
+      for (let str of strs) {
+        if (!str.startsWith(temporaryPrefix)) {
+          isEqual = false
+        }
+      }
+      if (!isEqual) {
+        break
+      } else {
+        prefix = temporaryPrefix
+      }
+    }
+    return prefix
+  }
+}
+
+export default longestCommonPrefix
